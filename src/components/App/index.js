70c4ff816ab586dd77d32b1a45fbfecad794e84f
Evaldas <32436432+EvaldasBurlingis@users.noreply.github.com>
2019-12-12 10:00:04 +0000

    changes
    styling main section

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Header from "../Header";
+import IconCloudy from "../../icons/cloudy";
 
 const BigScreen = () => <div>This works best on mobile</div>;
 
 const MobileApp = () => (
   <>
     <Header />
-    <h1>Hello from app</h1>
+    <main>
+      <section className="current">
+        <h2 className="current--title">Today</h2>
+        <div className="current--temperature">
+          <IconCloudy />
+          <span className="current--temp-big">38</span>
+        </div>
+        <p className="current--description">Partly Cloudy</p>
+        <div className="current-details">
+          <div className="current-details--box">
+            <span className="current-details--detail">11km/h</span>
+            <span className="current-details--desc">Wind</span>
+          </div>
+          <div className="current-details--box">
+            <span className="current-details--detail">78%</span>
+            <span className="current-details--desc">Humidity</span>
+          </div>
+          <div className="current-details--box">
+            <span className="current-details--detail">32</span>
+            <span className="current-details--desc">Feels Like</span>
+          </div>
+        </div>
+      </section>
+    </main>
   </>
 );
 
