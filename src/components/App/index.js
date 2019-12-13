6e30e4753bdf77f3c9ad4ae8495de964c1c232c9
Evaldas <32436432+EvaldasBurlingis@users.noreply.github.com>
2019-12-13 10:42:37 +0000

    cors sandbox err

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,35 +4,41 @@ import IconCloudy from "../../icons/cloudy";
 
 const BigScreen = () => <div>This works best on mobile</div>;
 
-const MobileApp = ({ weatherData }) => (
-  <>
-    <Header city={weatherData.name} />
-    <main>
-      <section className="current">
-        <h2 className="current--title">Today</h2>
-        <div className="current--temperature">
-          <IconCloudy />
-          <span className="current--temp-big">38</span>
-        </div>
-        <p className="current--description">Partly Cloudy</p>
-        <div className="current-details">
-          <div className="current-details--box">
-            <span className="current-details--detail">11km/h</span>
-            <span className="current-details--desc">Wind</span>
+const MobileApp = ({ data }) => {
+  console.log(data.wind.speed);
+
+  return (
+    <>
+      <Header city={data.name} />
+      <main>
+        <section className="current">
+          <h2 className="current--title">Today</h2>
+          <div className="current--temperature">
+            <IconCloudy />
+            <span className="current--temp-big">
+              {Math.round(data.main.temp)}&deg;
+            </span>
           </div>
-          <div className="current-details--box">
-            <span className="current-details--detail">78%</span>
-            <span className="current-details--desc">Humidity</span>
+          <p className="current--description">Partly Cloudy</p>
+          <div className="current-details">
+            <div className="current-details--box">
+              <span className="current-details--detail">11km/h</span>
+              <span className="current-details--desc">Wind</span>
+            </div>
+            <div className="current-details--box">
+              <span className="current-details--detail">78%</span>
+              <span className="current-details--desc">Humidity</span>
+            </div>
+            <div className="current-details--box">
+              <span className="current-details--detail">32</span>
+              <span className="current-details--desc">Feels Like</span>
+            </div>
           </div>
-          <div className="current-details--box">
-            <span className="current-details--detail">32</span>
-            <span className="current-details--desc">Feels Like</span>
-          </div>
-        </div>
-      </section>
-    </main>
-  </>
-);
+        </section>
+      </main>
+    </>
+  );
+};
 
 const App = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -53,8 +59,7 @@ const App = () => {
     fetchData();
   }, []);
 
-  console.log(weatherData);
-  return width > 450 ? <BigScreen /> : <MobileApp weatherData={weatherData} />;
+  return width > 450 ? <BigScreen /> : <MobileApp data={weatherData} />;
 };
 
 export default App;
