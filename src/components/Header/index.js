c5bc4cff2caf4a48edd62eb8e29197f915b847ac
Evaldas <evabur@icloud.com>
2019-12-16 10:27:20 +0000

    search overlay

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,17 +3,31 @@ import moment from "moment";
 import { IconSearch } from "../../icons";
 import PropTypes from 'prop-types';
 
+const SearchInput = ({ setIsSearchOpen }) => {
+  return (
+    <div className="search">
+      <div className="search--header">
+        <button
+          className="search--btn__close"
+          onClick={() => setIsSearchOpen(false)}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
 const Header = ({ city }) => {
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(true);
 
   const toggleSearch = e => {
     setIsSearchOpen(!isSearchOpen)
-
-    console.log(isSearchOpen)
     e.preventDefault();
   }
   return (
     <header className="header">
+      {isSearchOpen && <SearchInput setIsSearchOpen={setIsSearchOpen}/>}
       <div className="header--main">
         <span className="header--city">{city}</span>
         <span onClick={toggleSearch} style={{display: "flex", alignItems: "center"}}><IconSearch /></span>
@@ -28,4 +42,8 @@ export default Header;
 
 Header.propTypes = {
   city: PropTypes.string
+}
+
+SearchInput.propTypes = {
+  setIsSearchOpen: PropTypes.func
 }
